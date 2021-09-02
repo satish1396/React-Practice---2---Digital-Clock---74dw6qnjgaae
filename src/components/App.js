0a804cb906602f3537a0efbbf4c63f070edee635
satish1396 <satyanarayanbingi14@gmail.com>
2021-09-03 00:50:21 +0530

    fix problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+function getTimefun(){
+    let date = new Date()
+    return date.toLocaleTimeString()
+}
 
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           time:""      
+        }
+    }
+    componentDidMount(){
+    setInterval(()=>{
+        this.setState({
+            time : getTimefun()
+        })
+    })
+    }
+    
+    render() {
         return(
-            <>
-               
-            </>
+            <div>
+               <h3 id="time">{this.state.time}</h3>
+            </div>
         )
     }
 }
